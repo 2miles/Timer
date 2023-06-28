52d62956330cb0b02949eb01f3e5ffb75a913c74
miles <miles4599@gmail.com>
2023-06-28 01:23:16 -0700

    highlight the best and worst laps

--- src/components/Laps.js
+++ src/components/Laps.js
@@ -7,6 +7,16 @@ import {
 } from '../utils';
 
 function Laps({ laps, totals }) {
+  const bestLap = (laps) => {
+    return laps.indexOf(Math.min(...laps));
+  };
+
+  const worstLap = (laps) => {
+    return laps.indexOf(Math.max(...laps));
+  };
+
+  let lapClassName = '';
+
   return (
     <div className="laps-area">
       <div className="laps-header">
@@ -15,12 +25,18 @@ function Laps({ laps, totals }) {
         <div>Total Time</div>
       </div>
       {laps.map((lap, index) => {
+        index === bestLap(laps) && laps.length > 1
+          ? (lapClassName = 'best-lap-time')
+          : index === worstLap(laps) && laps.length > 1
+          ? (lapClassName = 'worst-lap-time')
+          : (lapClassName = '');
+
         return (
           <div className="lap">
             <div className="lap-title">
               <span>{`${index + 1}`}</span>
             </div>
-            <div>
+            <div className={lapClassName}>
               <span>{displayHours(lap)}</span>:
               <span>{displayMinutes(lap)}</span>:
               <span>{displaySeconds(lap)}</span>.
@@ -29,7 +45,7 @@ function Laps({ laps, totals }) {
               </span>
             </div>
             <div>
-              <span>{displayHours(lap)}</span>:
+              <span>{displayHours(totals[index])}</span>:
               <span>{displayMinutes(totals[index])}</span>:
               <span>{displaySeconds(totals[index])}</span>.
               <span style={{ fontSize: '.9rem', alignSelf: 'flex-end' }}>
